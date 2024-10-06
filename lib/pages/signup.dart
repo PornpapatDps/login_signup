db52dc2e969a04904f989e8156fd87cf81021a1a
Pornpapat <pornpapat.d@ku.th>
2024-10-06 18:42:14 +0700

    Update signup.dart

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,162 +1,367 @@
-// import 'package:flutter/material.dart';
-// import 'package:test_cloudbase/main.dart';
-// import '../database/auth.dart'; // Import the authentication helper
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-// class SignUpScreen extends StatefulWidget {
-//   const SignUpScreen({Key? key}) : super(key: key);
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
 
-//   @override
-//   _SignUpScreenState createState() => _SignUpScreenState();
-// }
+class _SignUpScreenState extends State<SignUpScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  String errorMessage = '';
+  bool _isPasswordVisible = false; // State variable for password visibility
+  
+bool _validateInput() {
+  if (_emailController.text.isEmpty || !_emailController.text.contains('@')) {
+    _showErrorDialog(context, 'กรุณาใส่อีเมลที่ถูกต้อง');
+    return false;
+  }
+  if (_passwordController.text.length < 6) {
+    _showErrorDialog(context, 'รหัสผ่านต้องมีความยาวอย่างน้อย 6 ตัว');
+    return false;
+  }
+  return true;
+}
 
-// class _SignUpScreenState extends State<SignUpScreen> {
-//   String _email = '';
-//   String _passwd = '';
-//   String _confirmPasswd = '';
+Future<void> _register() async {
+  if (!_validateInput()) return; // ตรวจสอบข้อมูล
 
-//   Future _showAlert(BuildContext context, String message) {
-//     return showDialog(
-//         context: context,
-//         builder: (BuildContext context) {
-//           return AlertDialog(
-//             title: Text(
-//               message,
-//               style: const TextStyle(fontSize: 16),
-//             ),
-//             shape: const RoundedRectangleBorder(
-//               borderRadius: BorderRadius.all(Radius.circular(18.0)),
-//             ),
-//             actions: [
-//               ElevatedButton(
-//                 child: const Text('Close'),
-//                 onPressed: () {
-//                   Navigator.pop(context);
-//                 },
-//               )
-//             ],
-//           );
-//         });
-//   }
+  try {
+    await _auth.createUserWithEmailAndPassword(
+      email: _emailController.text,
+      password: _passwordController.text,
+    );
+    
+    // แจ้งเตือนว่า "ลงทะเบียนสำเร็จแล้ว"
+    _showSuccessDialog(context, "ลงทะเบียนสำเร็จแล้ว");
+    
+    // นำทางไปยังหน้าล็อกอินหรือหน้าหลัก
+    // Navigator.of(context).pop(); // Uncomment if you want to navigate after showing the success dialog
+  } on FirebaseAuthException catch (e) {
+    // แสดงข้อความผิดพลาด
+    _showErrorDialog(context, e.message ?? "เกิดข้อผิดพลาด");
+  }
+}
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: Container(
-//           color: Colors.white,
-//           child: Padding(
-//             padding: const EdgeInsets.all(36.0),
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.center,
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 const SizedBox(
-//                   height: 155.0,
-//                   child: Text(
-//                     'Sign Up',
-//                     style: TextStyle(fontSize: 30),
-//                   ),
-//                 ),
-//                 const SizedBox(height: 45.0),
-//                 TextFormField(
-//                   key: UniqueKey(),
-//                   obscureText: false,
-//                   initialValue: _email,
-//                   autofocus: true,
-//                   onChanged: (value) => _email = value,
-//                   decoration: InputDecoration(
-//                       contentPadding:
-//                           const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-//                       hintText: "Email",
-//                       border: OutlineInputBorder(
-//                           borderRadius: BorderRadius.circular(32.0))),
-//                 ),
-//                 const SizedBox(height: 25.0),
-//                 TextFormField(
-//                   key: UniqueKey(),
-//                   obscureText: true,
-//                   initialValue: _passwd,
-//                   onChanged: (value) => _passwd = value,
-//                   decoration: InputDecoration(
-//                       contentPadding:
-//                           const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-//                       hintText: "Password",
-//                       border: OutlineInputBorder(
-//                           borderRadius: BorderRadius.circular(32.0))),
-//                 ),
-//                 const SizedBox(height: 25.0),
-//                 TextFormField(
-//                   key: UniqueKey(),
-//                   obscureText: true,
-//                   initialValue: _confirmPasswd,
-//                   onChanged: (value) => _confirmPasswd = value,
-//                   decoration: InputDecoration(
-//                       contentPadding:
-//                           const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-//                       hintText: "Confirm Password",
-//                       border: OutlineInputBorder(
-//                           borderRadius: BorderRadius.circular(32.0))),
-//                 ),
-//                 const SizedBox(
-//                   height: 35.0,
-//                 ),
-//                 Material(
-//                   elevation: 5.0,
-//                   borderRadius: BorderRadius.circular(30.0),
-//                   color: Colors.deepPurpleAccent,
-//                   child: MaterialButton(
-//                     minWidth: MediaQuery.of(context).size.width,
-//                     padding:
-//                         const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-//                     onPressed: () async {
-//                       if (_passwd == _confirmPasswd) {
-//                         // Register new user
-//                         await UserAuthenticator()
-//                             .registerWithEmailAndPassword(_email, _passwd)
-//                             .then((res) {
-//                           if (res == true) {
-//                             _showAlert(context, "Registration successful!");
-//                             setState(() {
-//                               _email = '';
-//                               _passwd = '';
-//                               _confirmPasswd = '';
-//                             });
-//                           } else {
-//                             _showAlert(context, "Registration failed!");
-//                           }
-//                         });
-//                       } else {
-//                         _showAlert(context, "Passwords do not match!");
-//                       }
-//                     },
-//                     child: const Text(
-//                       "Sign Up",
-//                       textAlign: TextAlign.center,
-//                       style: TextStyle(color: Colors.white),
-//                     ),
-//                   ),
-//                 ),
-//                 const SizedBox(
-//                   height: 15.0,
-//                 ),
-//                 TextButton(
-//                   onPressed: () {
-//                     Navigator.push(
-//                       context,
-//                       MaterialPageRoute(
-//                           builder: (context) => const LoginScreen()),
-//                     );
-//                   },
-//                   child: const Text(
-//                     " have an account Login",
-//                     style: TextStyle(color: Colors.deepPurpleAccent),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+
+
+Future _showErrorDialog(BuildContext context, String message) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          contentPadding:
+              EdgeInsets.zero, // ใช้เป็น 0 เพื่อให้ใช้ได้ทั้ง Container
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(18.0)),
+          ),
+          content: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF0A2342),
+                  Color(0xFF1E3A5F)
+                ], // ไล่สีการ์ดใน Alert
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(18.0)),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min, // ขนาดของ Alert ใหญ่ตามเนื้อหา
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    message,
+                    style: const TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF1E88E5),
+                      ),
+                      child: const Text(
+                        'ปิด',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+Future _showSuccessDialog(BuildContext context, String message) {
+  return showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        contentPadding: EdgeInsets.zero,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(18.0)),
+        ),
+        content: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF0A2342),
+                Color(0xFF1E3A5F),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(18.0)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  message,
+                  style: const TextStyle(fontSize: 16, color: Colors.white),
+                ),
+                const SizedBox(height: 20.0),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF1E88E5),
+                    ),
+                    child: const Text(
+                      'ปิด',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.of(context).pop(); // นำทางกลับไปยังหน้าล็อกอินหรือหน้าหลัก
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    },
+  );
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF1C2833),
+              Color(0xFF2C3E50),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Container(
+            margin: const EdgeInsets.all(20.0),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF0A2342),
+                  Color(0xFF1E3A5F),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.circular(25.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.4),
+                  blurRadius: 15,
+                  offset: const Offset(0, 8),
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.account_balance_wallet,
+                    size: 80,
+                    color: Colors.white,
+                  ),
+                  const SizedBox(height: 20.0),
+                  const Text(
+                    'Wallets+',
+                    style: TextStyle(
+                      fontSize: 28,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Text(
+                    'สร้างบัญชีผู้ใช้ใหม่',
+                    style: TextStyle(
+                      fontSize: 28,
+                      color: Colors.white70,
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  _buildTextField(
+                    label: 'Email',
+                    controller: _emailController,
+                    obscureText: false,
+                    icon: Icons.email,
+                  ),
+                  const SizedBox(height: 20.0),
+                  _buildPasswordField(
+                    label: 'Password',
+                    controller: _passwordController,
+                    icon: Icons.lock,
+                  ),
+                  const SizedBox(height: 30.0),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: _register,
+                      child: const Text(
+                        'ลงทะเบียน',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        backgroundColor: const Color(0xFF1E88E5),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerRight,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text(
+                      "มีบัญชีผู้ใช้แล้ว? กดเพื่อเข้าสู่ระบบ",
+                      style: TextStyle(
+                        color: Colors.white70,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField({
+    required String label,
+    required TextEditingController controller,
+    required bool obscureText,
+    required IconData icon,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(color: Colors.white),
+        ),
+        const SizedBox(height: 8.0),
+        TextFormField(
+          controller: controller,
+          obscureText: obscureText,
+          style: const TextStyle(color: Colors.white), // Set text color to white
+          decoration: InputDecoration(
+            prefixIcon: Icon(icon, color: Colors.white),
+            filled: true,
+            fillColor: Colors.white.withOpacity(0.1),
+            contentPadding: const EdgeInsets.symmetric(vertical: 16.0),
+            hintText: "Enter your $label",
+            hintStyle: const TextStyle(color: Colors.white54),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0),
+              borderSide: BorderSide.none,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPasswordField({
+    required String label,
+    required TextEditingController controller,
+    required IconData icon,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(color: Colors.white),
+        ),
+        const SizedBox(height: 8.0),
+        TextFormField(
+          controller: controller,
+          obscureText: !_isPasswordVisible,
+          style: const TextStyle(color: Colors.white), // Set text color to white
+          decoration: InputDecoration(
+            prefixIcon: Icon(icon, color: Colors.white),
+            suffixIcon: IconButton(
+              icon: Icon(
+                _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                setState(() {
+                  _isPasswordVisible = !_isPasswordVisible; // Toggle password visibility
+                });
+              },
+            ),
+            filled: true,
+            fillColor: Colors.white.withOpacity(0.1),
+            contentPadding: const EdgeInsets.symmetric(vertical: 16.0),
+            hintText: "Enter your $label",
+            hintStyle: const TextStyle(color: Colors.white54),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0),
+              borderSide: BorderSide.none,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
